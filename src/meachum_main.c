fd402dfdb9c34b3a013fad056acca5706169958f
smudugal <warren.samyukta@gmail.com>
2016-02-08 20:51:39 -0700

    Adds bitmap image and time layer together

--- src/meachum_main.c
+++ src/meachum_main.c
@@ -22,22 +22,23 @@ static void main_window_load(Window *window) {
   GRect layer_bounds = layer_get_bounds(root_layer);
   
   pic_bitmap = gbitmap_create_with_resource(RESOURCE_ID_WIFEY_PIC);
-  bitmap_layer = bitmap_layer_create(GRect(5, 5, 48, 48));
+  bitmap_layer = bitmap_layer_create(GRect(35, 10, 72, 72));
   bitmap_layer_set_bitmap(bitmap_layer, pic_bitmap);
+
   
-//   time_layer = text_layer_create(
-//     GRect(0, PBL_IF_ROUND_ELSE(58,52), layer_bounds.size.w, layer_bounds.size.h));
+  time_layer = text_layer_create(
+    GRect(0, PBL_IF_ROUND_ELSE(58,80), layer_bounds.size.w, layer_bounds.size.h));
   
-//   text_layer_set_background_color(time_layer, GColorClear);
-//   text_layer_set_text_color(time_layer, GColorBlack);
-//   text_layer_set_font(time_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28 ));
-//   text_layer_set_text_alignment(time_layer, GTextAlignmentCenter);
-//   text_layer_set_overflow_mode(time_layer, GTextOverflowModeWordWrap);
+  text_layer_set_background_color(time_layer, GColorClear);
+  text_layer_set_text_color(time_layer, GColorBlack);
+  text_layer_set_font(time_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28 ));
+  text_layer_set_text_alignment(time_layer, GTextAlignmentCenter);
+  text_layer_set_overflow_mode(time_layer, GTextOverflowModeWordWrap);
 
   layer_add_child(root_layer, bitmap_layer_get_layer(bitmap_layer));
-  //layer_add_child(bitmap_layer_get_layer(bitmap_layer), text_layer_get_layer(time_layer));
+  layer_add_child(root_layer, text_layer_get_layer(time_layer));
   
-  //update_time();
+  update_time();
 }
 
 static void main_window_unload(Window *window) {
@@ -47,7 +48,7 @@ static void main_window_unload(Window *window) {
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
-  //update_time();
+  update_time();
 }
 
 void handle_init(void) {
